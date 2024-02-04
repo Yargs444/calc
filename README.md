77308cf604ebb477c50d2094bc5b3771ded2a4a6
dudushy <eduardotalarico1337@gmail.com>
2024-02-03 23:42:07 -0300

    Remove old stuff and create new ones

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# calc
+[![pages-build-deployment](https://github.com/dudushy/calc/actions/workflows/pages/pages-build-deployment/badge.svg?branch=main)](https://github.com/dudushy/calc/actions/workflows/pages/pages-build-deployment)
+
+# [calc](https://github.com/dudushy/calc/)
 Calculator.
--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,26 @@
-console.log('backend');
+const TITLE = "calc";
+console.log(`[${TITLE}] init`);
 
-// eslint-disable-next-line no-unused-vars
-function test(event) {
-  console.log(event);
+const rotateDelay = 50;
+console.log(`[${TITLE}] rotateDelay`, rotateDelay);
+
+rotateBackground(false);
+
+function rotateBackground(log = false) {
+  const body = document.querySelector("body");
+  if (log) console.log(`[${TITLE}#rotateBackground] body`, body);
+
+  const lastDeg = Number(localStorage.getItem("lastDeg") || '0');
+  if (log) console.log(`[${TITLE}#rotateBackground] lastDeg`, lastDeg);
+
+  const newDeg = lastDeg + 1 > 360 ? 0 : lastDeg + 1;
+  if (log) console.log(`[${TITLE}#rotateBackground] newDeg`, newDeg);
+
+  localStorage.setItem("lastDeg", newDeg);
+
+  body.style.background = `linear-gradient(${newDeg}deg, var(--tertiary), var(--danger))`;
+
+  setTimeout(() => {
+    rotateBackground(log);
+  }, rotateDelay);
 }
