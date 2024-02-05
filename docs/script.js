5869a30cb1d9b4a0514fab8052e90dde35f7a49e
dudushy <eduardotalarico1337@gmail.com>
2024-02-05 00:40:01 -0300

    Create some functions

--- docs/script.js
+++ docs/script.js
@@ -6,6 +6,11 @@ console.log(`[${TITLE}] rotateDelay`, rotateDelay);
 
 rotateBackground(false);
 
+const display = document.getElementById("displaySpan");
+console.log(`[${TITLE}] display`, display);
+
+let usedDot = false;
+
 function rotateBackground(log = false) {
   const body = document.querySelector("body");
   if (log) console.log(`[${TITLE}#rotateBackground] body`, body);
@@ -23,4 +28,46 @@ function rotateBackground(log = false) {
   setTimeout(() => {
     rotateBackground(log);
   }, rotateDelay);
+}
+
+function openSource() {
+  console.log(`[${TITLE}#openSource]`);
+
+  window.open("https://github.com/dudushy/calc");
+}
+
+function clearDisplay() {
+  console.log(`[${TITLE}#clear] display.innerText`, display.innerText);
+
+  display.innerText = "0";
+}
+
+function dot() {
+  console.log(`[${TITLE}#dot] usedDot`, usedDot);
+  if (display.innerText.includes(".")) return;
+  else if (usedDot) return;
+
+  display.innerText += ".";
+  usedDot = true;
+}
+
+function appendNumber(number) {
+  console.log(`[${TITLE}#appendNumber] number`, number);
+
+  if (display.innerText == "0" && number == "0") return;
+  else if (display.innerText == "0") display.innerText = number;
+  else display.innerText += number;
+}
+
+function deleteLast() {
+  console.log(`[${TITLE}#deleteLast] display.innerText`, display.innerText);
+
+  if (display.innerText.length == 1) display.innerText = "0";
+  else display.innerText = display.innerText.slice(0, -1);
+}
+
+function invert() {
+  console.log(`[${TITLE}#invert] display.innerText`, display.innerText);
+
+  display.innerText = Number(display.innerText) * -1;
 }
